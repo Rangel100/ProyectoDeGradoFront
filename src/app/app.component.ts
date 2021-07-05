3f58b332e601ff1164cc8b5997b766ac3a689e27
Jhonatan Rangel <jhonatan-rangel99@hotmail.com>
2021-07-04 19:09:47 -0500

    Inicio de aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TipoUsuarioService } from './services/tipo-usuario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pdg';
+
+  constructor (
+    private tipoUsuarioService:TipoUsuarioService,
+  ){}
+
+  ngOnInit(): void {
+
+    this.tipoUsuarioService.consultarTiposUsuario().subscribe(data=>{
+      console.log(data);
+    });
+    
+  }
 }
---
+++ src/app/services/tipo-usuario.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TipoUsuarioService {
+
+  url: string;
+
+  constructor(
+    private httpClient: HttpClient,
+  ) {
+    this.url = environment.URL + 'tipoUsuario/'
+  }
+
+  public consultarTiposUsuario(): Observable<any> {
+    return this.httpClient.get(this.url);
+  }
+}
